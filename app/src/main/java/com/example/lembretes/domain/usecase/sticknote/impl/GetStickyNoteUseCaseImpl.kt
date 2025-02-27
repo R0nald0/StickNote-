b6182c59909aa77426ae029dc697fd74a54322be
Ronaldo Anjos <roanjos499@gmail.com>
2025-02-26 23:43:08 -0300

    fix do erro alteraçao de busca de datas e resolução lista vindo vazia para datas de hoje amanha

--- app/src/main/java/com/example/lembretes/domain/usecase/sticknote/impl/GetStickyNoteUseCaseImpl.kt
+++ app/src/main/java/com/example/lembretes/domain/usecase/sticknote/impl/GetStickyNoteUseCaseImpl.kt
@@ -1,14 +1,16 @@
 package com.example.lembretes.domain.usecase.sticknote.impl
 
+import android.util.Log
 import com.example.lembretes.domain.model.StickyNoteDomain
 import com.example.lembretes.domain.repository.StickyNoteRepository
 import com.example.lembretes.domain.usecase.sticknote.GetStickyNoteUseCase
-import com.example.lembretes.utils.convertDateStringToLong
 import com.example.lembretes.utils.dateTimeTomorow
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Instant
 import java.util.Calendar
 import java.util.Date
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.days
 
 class GetStickyNoteUseCaseImpl @Inject constructor(
     private  val stickyNoteRepository: StickyNoteRepository
@@ -40,9 +42,13 @@ class GetStickyNoteUseCaseImpl @Inject constructor(
 
         val firsEPock =  Date().dateTimeTomorow(currenDate.timeInMillis,0)
         val secondEPock =   Date().dateTimeTomorow(finalDate.timeInMillis,0)
+
+        Log.i("_INFO", "getStickNotesToday:First $firsEPock ")
+        Log.i("_INFO", "getStickNotesToday:Second $secondEPock ")
+
         return  stickyNoteRepository.getStickByPeriodicDate(
-            firstDate = Date().convertDateStringToLong(firsEPock)!!,
-            secondDate =  Date().convertDateStringToLong(secondEPock)!!
+            firstDate = currenDate.timeInMillis,
+            secondDate =  finalDate.timeInMillis
         )
     }
 
@@ -65,13 +71,14 @@ class GetStickyNoteUseCaseImpl @Inject constructor(
                 59,
                 59)
         }
-       val firsEPock =  Date().dateTimeTomorow(newCurrenDate.timeInMillis,1)
-        val secondEPock =   Date().dateTimeTomorow(finalDate.timeInMillis,1)
-
+       val firsEPock = Instant.fromEpochMilliseconds(newCurrenDate.timeInMillis)
+             .plus(1.days).toEpochMilliseconds()
+        val secondEPock =  Instant.fromEpochMilliseconds(finalDate.timeInMillis)
+            .plus(1.days).toEpochMilliseconds()
 
         return  stickyNoteRepository.getStickByPeriodicDate(
-           firstDate = Date().convertDateStringToLong(firsEPock)!!,
-           secondDate =  Date().convertDateStringToLong(secondEPock)!!
+           firstDate = firsEPock,
+           secondDate =  secondEPock
         )
     }
 
